f2003dd454c3a497067bff32321de1abb89d4dd8
Scott Hall <scotthall127@gmail.com>
2018-07-16 22:53:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array_reverse = []
   array = string.split(" ")
-  array.each do |word|
-    array_reverse << word.reverse
+  array.collect do |word|
+    word.reverse
   end
   array_reverse.join(" ")
 end
